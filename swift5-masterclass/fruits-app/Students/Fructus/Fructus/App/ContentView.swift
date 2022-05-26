679e82c8565cd55733f02643f9182a5f5c310101
Alessandro Aiezza <a.aiezza@gmail.com>
2022-05-26 15:32:51 -0400

    Lesson 6.46

--- swift5-masterclass/fruits-app/Students/Fructus/Fructus/App/ContentView.swift
+++ swift5-masterclass/fruits-app/Students/Fructus/Fructus/App/ContentView.swift
@@ -3,23 +3,36 @@ import SwiftUI
 struct ContentView: View {
   // MARK: - PROPERTIES
 
+  @State private var isShowingSettings: Bool = false
+
   var fruits: [Fruit] = fruitsData
 
   // MARK: - BODY
 
-    var body: some View {
-      NavigationView {
-        List {
-          ForEach(fruits.shuffled()) { fruit in
-            NavigationLink(destination: FruitDetailView(fruit: fruit)) {
-              FruitRowView(fruit: fruit)
-                .padding(.vertical, 4)
-            } //: NAVIGATION LINK
-          } //: FOR EACH FRUIT
-        } //: LIST
-        .navigationTitle(Text("Fruits"))
-      } //: NAVIGATION
-    }
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(fruits.shuffled()) { fruit in
+          NavigationLink(destination: FruitDetailView(fruit: fruit)) {
+            FruitRowView(fruit: fruit)
+              .padding(.vertical, 4)
+          } //: NAVIGATION LINK
+        } //: FOR EACH FRUIT
+      } //: LIST
+      .navigationTitle(Text("Fruits"))
+      .navigationBarItems(
+        trailing:
+          Button(action: {
+            isShowingSettings = true
+          }) {
+            Image(systemName: "slider.horizontal.3")
+          } //: BUTTON
+          .sheet(isPresented: $isShowingSettings) {
+            SettingsView()
+          }
+      )
+    } //: NAVIGATION
+  }
 }
 
 // MARK: - PREVIEW
